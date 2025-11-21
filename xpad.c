e1f44d4f40594cb14e0ab7413e5f75423911dabc
RayJW <29835902+RayJW@users.noreply.github.com>
2025-11-21 14:09:28 +0100

    Input: add support for EasySMX D10 controller Signed-off-by: Jeffrey Wälti <jeffrey@waelti.dev>

--- xpad.c
+++ xpad.c
@@ -386,6 +386,7 @@ static const struct xpad_device {
 	{ 0x20d6, 0x2009, "PowerA Enhanced Wired Controller for Xbox Series X|S", 0, XTYPE_XBOXONE },
 	{ 0x20d6, 0x281f, "PowerA Wired Controller For Xbox 360", 0, XTYPE_XBOX360 },
 	{ 0x2345, 0xe00b, "Machenike G5 Pro Controller", 0, XTYPE_XBOX360 },
+	{ 0x2345, 0xe062, "EasySMX D10", 0, XTYPE_XBOX360 },
 	{ 0x24c6, 0x5000, "Razer Atrox Arcade Stick", MAP_TRIGGERS_TO_BUTTONS, XTYPE_XBOX360 },
 	{ 0x24c6, 0x5300, "PowerA MINI PROEX Controller", 0, XTYPE_XBOX360 },
 	{ 0x24c6, 0x5303, "Xbox Airflo wired controller", 0, XTYPE_XBOX360 },
